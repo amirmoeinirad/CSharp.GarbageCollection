3925d85a957f7567dceb083fdc26005c35c467c6
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-08-13 15:47:25 +0330

    Add project files.

---
+++ Garbage Collection/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Main Concept: Garbage Collection (GC) in .NET
+
+namespace GarbageCollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Garbage Collection in .NET...");
+            Console.WriteLine("-----------------------------\n");
+        }
+    }
+}
